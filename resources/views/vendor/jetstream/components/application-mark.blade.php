40df920570726c338b9c08999b3fb93f9c2008b6
Richard Muragijimana <beastar457@gmail.com>
2022-01-25 15:03:21 +0200

    init

---
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -0,0 +1,3 @@
+<!-- use image to show image from public folder -->
+
+<img src="{{ asset('images/favicon.png') }}" height="30" width="30">
---
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -0,0 +1,75 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+
+    <div class="mt-8 text-2xl">
+        <div class="flex flex-wrap mx-auto">
+            <a class="
+              inline-flex
+              items-center
+              justify-center
+              w-1/2
+              py-3
+              font-medium
+              leading-none
+              tracking-wider
+              text-indigo-500
+              bg-gray-100
+              border-b-2 border-indigo-500
+              rounded-t
+              sm:px-6 sm:w-auto sm:justify-start
+              title-font
+            ">
+                Pending
+            </a>
+            <a class="
+              inline-flex
+              items-center
+              justify-center
+              w-1/2
+              py-3
+              font-medium
+              leading-none
+              tracking-wider
+              border-b-2 border-gray-200
+              sm:px-6 sm:w-auto sm:justify-start
+              title-font
+              hover:text-gray-900
+            ">
+                Approved
+            </a>
+
+        </div>
+        <div class="mt-8 text-2xl">
+            Welcome irembo user management system
+        </div>
+
+        <div class="mt-6 text-gray-500">
+            You can change your user info in user profile management Menu.
+        </div>
+    </div>
+
+    <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+        <div class="p-6">
+
+
+            <div class="ml-12">
+                <div class="mt-2 text-sm text-gray-500">
+                    Irembo user management system!
+                </div>
+
+                <a href="#">
+                    <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
+                        <div>Explore</div>
+
+                        <div class="ml-1 text-indigo-500">
+                            <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+                                <path fill-rule="evenodd"
+                                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    </div>
